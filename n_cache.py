920fe4bffaf4f356ebb63b7aefd6c9c607e7d65f
mojozinc <jangra.1202@gmail.com>
2020-06-05 01:54:53 +0530

    synchronize read and write method

--- n_cache.py
+++ n_cache.py
@@ -1,7 +1,9 @@
 """Multi level cache"""
+import functools
+import threading
 import warnings
 from collections import OrderedDict
-from pprint import pprint
+from pprint import pprint, pformat
 from time import sleep
 
 import yaml
@@ -15,6 +17,7 @@ WRITE_TIME_PER_LEVEL = [0.1, 0.2, 0.3, 0.4]
 
 class LruCache:
     """least recently used, cache"""
+
     def __init__(self, capacity=0):
         self.capacity = capacity
         self.cache = OrderedDict()
@@ -23,7 +26,7 @@ class LruCache:
         """read value of key, return none if cache miss"""
         if key not in self.cache:
             return None
-        self.cache.move_to_end(key)  # make key hot
+        self._touch_(key)
         return self.cache[key]
 
     def write(self, key, value):
@@ -37,9 +40,21 @@ class LruCache:
             popped = self.cache.popitem(last=False)
         return popped
 
+    def _touch_(self, key, value=None):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        elif value is not None:
+            self.write(key, value)
+
+    def __str__(self):
+        return pformat(dict(self.cache), indent=4)
+
+    def __repr__(self):
+        return str(self)
 
 class NCache:
     """Multi level cache"""
+
     strategy_to_class = dict(lru=LruCache)
 
     def __init__(self, config):
@@ -60,6 +75,8 @@ class NCache:
             self.cache_stack.append(cache_class(capacity=capacity))
         self.levels = len(self.cache_stack)
 
+        self.lock = threading.Lock()
+
     def _read_level_(self, level, key):
         value = self.cache_stack[level].read(key)
         sleep(READ_TIME_PER_LEVEL[level])
@@ -70,31 +87,44 @@ class NCache:
         sleep(WRITE_TIME_PER_LEVEL[level])
         return popped
 
-    def read(self, key, debug=False):
+    def _touch_level_(self, level, key, value=None):
+        self.cache_stack[level]._touch_(key, value)
+
+    def read(self, key):
         """Tries to read key from all levels,
         if found, updates all the lower cold caches
         """
-        for level in range(self.levels):
-            value = self._read_level_(level, key)
-            if value is not None:
-                # cache hit
-                # update all cold cache levels
-                for cold_level in range(level):
-                    self._write_level_(cold_level, key, value)
-                return value
-        # cache miss
-        return None
+        with self.lock:
+            value, level = None, 0
+            while level < self.levels and value is None:
+                value = self._read_level_(level, key)
+                level += 1
+            if value:
+                for level in range(self.levels):
+                    # touch keys in all levels, should become the hottest key
+                    # touch is a write without time penalty
+                    self._touch_level_(level, key, value)
+            # cache miss
+            return value
 
     def write(self, key, value):
         """writes a key value pair to all cache levels
         returns a dict of popped items, if any None if no items were popped
         """
-        popped = {}
-        for level in range(self.levels):
-            _popped_ = self._write_level_(level, key, value)
-            if _popped_:
-                popped[level] = _popped_
-        return popped or None
+        with self.lock:
+            popped = {}
+            for level in range(self.levels):
+                _popped_ = self._write_level_(level, key, value)
+                if _popped_:
+                    popped[level] = _popped_
+            return popped or None
+
+    def __str__(self):
+        return pformat(self.cache_stack, indent=4)
+
+    def __repr__(self):
+        return str(self)
+
 
 if __name__ == "__main__":
     with open("config.yml") as fp:
